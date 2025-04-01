27bf1d71bb6824c8272bac0d41e8416b09a606a5
Glitchface01 <Glitchface01@gmail.com>
2025-04-01 13:36:43 +1000

    Inline Markdown

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,42 +1,40 @@
 class HTMLNode:
-       
-    def __init__(self, tag = None, value = None, children = None , props = None):
+    def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
         self.children = children
         self.props = props
-    
+
     def to_html(self):
         raise NotImplementedError("to_html method not implemented")
 
     def props_to_html(self):
-        if not self.props:
+        if self.props is None:
             return ""
-        props_string = ""
-        for key, value in self.props.items():
-            props_string += f' {key}="{value}"'
-
-        return props_string
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
 
     def __repr__(self):
-        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
 
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
         super().__init__(tag, value, None, props)
-        if value is None:
-            raise ValueError("LeafNode must have a value")
-        
+
     def to_html(self):
         if self.value is None:
-            raise ValueError("should be a value")
-        
+            raise ValueError("invalid HTML: no value")
         if self.tag is None:
             return self.value
-        
-        props_str = self.props_to_html()
-        return f"<{self.tag}{props_str}>{self.value}</{self.tag}>"
-    
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
         super().__init__(tag, None, children, props)
