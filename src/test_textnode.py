036873e7f319a8dd93b6961d33a20b135a67de2d
Glitchface01 <Glitchface01@gmail.com>
2025-04-02 14:04:02 +1000

    Extract Markdown

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType, text_node_to_html_node
+from textnode import TextNode, TextType, text_node_to_html_node, extract_markdown_images, extract_markdown_links 
 
 
 class TestTextNode(unittest.TestCase):
@@ -30,6 +30,42 @@ class TestTextNode(unittest.TestCase):
             "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
         )
 
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+            )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "Here's a [link to a website](https://example.com)"
+        )
+        self.assertListEqual([("link to a website", "https://example.com")], matches)
+
+    def test_multiple_links(self):
+        text = "Here's [one link](https://example.com) and [another](https://test.com)"
+        matches = extract_markdown_links(text)
+        self.assertListEqual([
+            ("one link", "https://example.com"),
+            ("another", "https://test.com")
+        ], matches)
+
+    def test_no_links(self):
+        matches = extract_markdown_links("This text has no links in it")
+        self.assertListEqual([], matches)
+
+    def test_multiple_images(self):
+        text = "Here's ![one image](https://example.com/img1.jpg) and ![another](https://example.com/img2.jpg)"
+        matches = extract_markdown_images(text)
+        self.assertListEqual([
+            ("one image", "https://example.com/img1.jpg"),
+            ("another", "https://example.com/img2.jpg")
+        ], matches)
+
+    def test_no_images(self):
+        matches = extract_markdown_images("This text has no images in it")
+        self.assertListEqual([], matches)
+
 
 class TestTextNodeToHTMLNode(unittest.TestCase):
     def test_text(self):
@@ -53,6 +89,8 @@ class TestTextNodeToHTMLNode(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "b")
         self.assertEqual(html_node.value, "This is bold")
+
+
 
 
 if __name__ == "__main__":
